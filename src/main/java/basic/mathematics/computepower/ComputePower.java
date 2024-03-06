f7af841b8d679f0b96df4b65626d307a7584c8d9
Ravi Kishan <raviksn2@gmail.com>
2024-03-06 17:47:41 +0530

    added iterative compute power

--- src/main/java/basic/mathematics/computepower/ComputePower.java
+++ src/main/java/basic/mathematics/computepower/ComputePower.java
@@ -5,6 +5,9 @@ public class ComputePower {
     public static void main(String[] args){
         System.out.println(computePower(4,4));
         System.out.println(computePower(6,3));
+
+        System.out.println(iterativeComputePower(4,4));
+        System.out.println(iterativeComputePower(6,3));
     }
 
     /**
@@ -12,7 +15,7 @@ public class ComputePower {
      * If n is even then above will be result
      * If n is odd then final result will be result*x
      * T.C: O(log(n))
-     * S.C; O(1)
+     * S.C; O(log(n)) -- for recursive calls
      * @param x
      * @param n
      * @return
@@ -29,5 +32,25 @@ public class ComputePower {
         }else{
             return temp*x;
         }
+    }
+
+    /**
+     * For computing x^n we check binary representation of n if the bit is set we multiply x into existing res else ignore
+     * T.C: O(log(n))
+     * S.C; O(1)
+     * @param x
+     * @param n
+     * @return
+     */
+    static long iterativeComputePower(int x, int n){
+        int res = 1;
+        while(n > 0){
+            if(n % 2 != 0){
+                res = res*x;
+            }
+            x = x*x;
+            n = n/2;
+        }
+        return res;
     }
 }
